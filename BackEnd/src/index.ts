9bbeabdca7c25c632d431bbbca6445b3321bc2d7
Thiago Costa <thycscosta@gmail.com>
2024-09-22 16:38:52 -0300

    importando algumas dependencias e linkagem do arquivo (routes)

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -1,4 +1,4 @@
-import server from './server/Server';
-
-server.listen(3333, ()=> console.log('App rodando'));
+import server from "./server/Server";
+const PORT = 3333;
+server.listen(PORT, ()=> console.log(`Servidor rodando  http://localhost:${PORT}`));
 
